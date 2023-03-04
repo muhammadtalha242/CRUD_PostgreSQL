94efbd0cf8fc67c82c163329e93f1aa104eca469
muhammadtalha242 <m.talha2244@gmail.com>
2023-03-04 12:31:46 +0100

    Services

---
+++ Services/SuperHeroServices/ISuperHeroService.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUD_PostgreSQL.Services.SuperHeroServices
+{
+    public interface ISuperHeroService
+    {
+        Task<List<SuperHero>> GetSuperHeros();
+        SuperHero? GetSuperHero(int id);
+        List<SuperHero>? AddSuperHero(SuperHero superHero);
+        List<SuperHero>? UpdateSuperHero(int id, SuperHero superHero);
+        List<SuperHero>? DeleteSuper(int id);
+
+    }
+}
---
+++ Services/SuperHeroServices/SuperHeroService.cs
@@ -0,0 +1,65 @@
+﻿namespace CRUD_PostgreSQL.Services.SuperHeroServices
+{
+    public class SuperHeroService: ISuperHeroService
+    {
+        private static List<SuperHero> SuperHeros = new List<SuperHero> { new SuperHero { Id = 1, Name = "Spide Man", FirstName = "Peter", LastName = "Paker", Place = "New York" },
+        new SuperHero { Id = 2, Name = "Spide Man1", FirstName = "Peter", LastName = "Paker", Place = "New York" },
+        new SuperHero { Id = 3, Name = "Spide Man2", FirstName = "Peter", LastName = "Paker", Place = "New York" }};
+        
+        private readonly DataContext _context;
+
+        public SuperHeroService(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        public List<SuperHero>? AddSuperHero(SuperHero superHero)
+        {
+               SuperHeros.Add(superHero);
+
+            return SuperHeros;
+        }
+
+        public List<SuperHero>? DeleteSuper(int id)
+        {
+            var superHero = SuperHeros.Find(x => x.Id == id);
+            if(superHero is null)
+            {
+                return null;
+            }
+            SuperHeros.Remove(superHero);
+            return SuperHeros;
+
+        }
+
+        public SuperHero? GetSuperHero(int id)
+        {
+            var superHero = SuperHeros.Find(x => x.Id == id);
+            if (superHero is null)
+            {
+                return null;
+            }
+            return superHero;
+        }
+
+        public async Task<List<SuperHero>> GetSuperHeros()
+        {
+
+            var heroes = await _context.SuperHeroes.ToListAsync();
+            return heroes;
+
+        }
+
+        public List<SuperHero>? UpdateSuperHero(int id, SuperHero superHero)
+        {
+            var _superHero = SuperHeros.Find(x => x.Id == id);
+            if (superHero is null)
+            {
+                return null;
+            }
+            SuperHeros.Remove(_superHero);
+            SuperHeros.Add(superHero);
+            return SuperHeros;
+        }
+    }
+}
