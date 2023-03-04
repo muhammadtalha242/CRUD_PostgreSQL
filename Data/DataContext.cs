6c68a60d724291994ce2d393215d9829bbaab1f1
muhammadtalha242 <m.talha2244@gmail.com>
2023-03-04 12:32:49 +0100

    data context

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CRUD_PostgreSQL.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress01;Database=superherodb;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
