55f14a1ee066de58a5da796960d965d7eba2d610
muhammadtalha242 <m.talha2244@gmail.com>
2023-03-04 12:33:18 +0100

    service injection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+global using CRUD_PostgreSQL.Models;
+global using Microsoft.EntityFrameworkCore;
+global using CRUD_PostgreSQL.Data;
+global using CRUD_PostgreSQL.Services.SuperHeroServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
